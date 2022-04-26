ea1b1f656529fdadbb79c4ad0119b3fd050362ef
Ubuntu <ubuntu@ip-172-31-17-170.ec2.internal>
2022-04-26 13:45:13 +0000

    Added the arrays

---
+++ spliceAndDice.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <list>
+#include <vector>
+#include "h-files/colors.h"
+
+using std::string;
+using std::stoi;
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+// initializing variables
+string uString, finalString, newString, uSize, uCopies, answer, charArr;
+int copies, size, arryLength;
+bool errorLoop = true, answerLoop = true;
+
+
+
+void splitString() {
+	arryLength = uString.length();
+	charArr[arryLength];
+	charArr = uString;
+	for (int counter = 0; counter < uString.length(); counter++) {
+		newString = newString + charArr[counter];
+		cout << newString << endl;
+		
+	}
+
+}
+
+
+int main()
+{
+	do {
+        cout << BOLDWHITE << "Enter a word or phrase to be Splice&Diced: ";
+        cin >> uString;
+        while (errorLoop){
+        	cout << "Enter how many characters long you want the Splice&Diced word to be: ";
+        	cin >> uSize;
+        	
+        	cout << "Enter how many times you want the Splice&Diced word to repeat: ";
+        	cin >> uCopies;
+        	try{
+				splitString();
+        	} 
+        }
+	} while(answer == "y");
+}
